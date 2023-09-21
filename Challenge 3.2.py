7a92ea2b7468f68dbe4b42b72ae575698629461a
Lokesh <144093570+Lokeshxanime@users.noreply.github.com>
2023-09-21 13:17:50 +0530

    Challenge 3.2.py

--- Challenge 3.2.py
+++ Challenge 3.2.py
@@ -7,15 +7,12 @@ class Student:
 
 
 def sort_students(student_list):
-  # Sort the list of students in descending order of CGPA
   sorted_students = sorted(student_list,
                            key=lambda student: student.cgpa,
-                           reverse=True)
-  # Syntax - lambda arg:exp
+                           reverse=True) 
   return sorted_students
 
 
-# Example usage:
 students = [
     Student("Hari", "A123", 7.8),
     Student("Srikanth", "A124", 8.9),
@@ -25,7 +22,6 @@ students = [
 
 sorted_students = sort_students(students)
 
-# Print the sorted list of students
 for student in sorted_students:
   print("Name: {}, Roll Number: {}, CGPA: {}".format(student.name,
                                                      student.roll_number,
